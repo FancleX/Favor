e36f2fac4a61e89e34eeb60fbe7b48da1f9e4514
chen <fancle666@gmail.com>
2023-06-05 17:52:34 -0400

    added routers

--- App.tsx
+++ App.tsx
@@ -1,16 +1,33 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import Home from './pages/Home';
+import Message from './pages/Message';
+import Account from './pages/Account';
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar style='auto' />
+      <Drawer.Navigator initialRouteName='Home'>
+            <Drawer.Screen name='Home' component={Home} />
+            <Drawer.Screen name='Message' component={Message} />
+            <Drawer.Screen name='Account' component={Account} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
+  header: {
+    flex: 1,
+    borderWidth: 5,
+    backgroundColor: 'grey'
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
